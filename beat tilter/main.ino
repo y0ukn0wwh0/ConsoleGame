37a9d65b8eb7287e63060976361090b494504f2d
juliolima45115 <81526507+juliolima45115@users.noreply.github.com>
2021-03-29 13:00:37 +0100

    classe buttons adicionada

--- beat tilter/main.ino
+++ beat tilter/main.ino
@@ -1,50 +1,85 @@
 #include <Arduboy2.h>
-
-// make an instance of arduboy used for many functions
+#include <Button.h>
 Arduboy2 arduboy;
+bool menu = true;
+//char buttons[] = {'A', 'B', 'U', 'D', 'L', 'R'};
+unsigned long initTime;
+//char b = 'J';
+Button buttons[100];
+int idx = 0;
 
-
-// This function runs once in your game.
-// use it for anything that needs to be set only once in your game.
 void setup() {
-  // initiate arduboy instance
   arduboy.begin();
-
-  // here we set the framerate to 15, we do not need to run at
-  // default 60 and it saves us battery life
   arduboy.setFrameRate(15);
+  
+  initTime = millis();
 }
 
 
-// our main game loop, this runs once every cycle/frame.
-// this is where our game logic goes.
 void loop() {
-  // pause render until it's time for the next frame
   if (!(arduboy.nextFrame()))
     return;
 
-  // first we clear our screen to black
   arduboy.clear();
-
-  drawMenu();
   
-  // then we finaly we tell the arduboy to display what we just wrote to the display
+  buttonsPressed();
+
+  if(menu)
+    drawMenu();
+  else{
+    drawGame();
+    generateButton();
+    for(int i = 0; i < sizeof(buttons); i++){
+      buttons[i].display();
+      buttons[i].move();
+    }
+  }
+  
   arduboy.display();
 }
 
 void drawMenu(){
   arduboy.drawRoundRect(3, 6, 120, 52, 3, WHITE);
   
-  printCenteredX(arduboy.width()/2, 3, "BEAT TILTER", 0);
+  printCenteredX(arduboy.width()/2, 3, "BEAT TILTER");
   arduboy.drawPixel(30, 6, BLACK);
   
-  printCenteredX(arduboy.width()/2, 20, "START  A", 0);
+  printCenteredX(arduboy.width()/2, 20, "START  A");
   
   arduboy.drawCircle(84, 23, 6, WHITE);
 }
 
-void printCenteredX(int x, int y, char text[], int extrasize){
-  int totalX = strlen(text) * 6 + extrasize;
+void drawGame(){
+  arduboy.drawRect(1, 1, 25, 60, WHITE);
+  arduboy.drawRect(101, 1, 25, 60, WHITE);
+  
+  arduboy.drawCircle(13, 50, 5, WHITE);
+  arduboy.drawCircle(113, 50, 5, WHITE);
+  
+  printCenteredX(arduboy.width()/2, 20, String(b));
+}
+
+void generateButton(){
+  if(millis() - initTime >= 2000){
+    //int i = random(0, 6);
+    Button button();
+    buttons[idx] = button;
+    idx++;
+    initTime = millis();
+    //b = buttons[i];
+  }
+}
+
+void printCenteredX(int x, int y, String text){
+  int totalX = text.length() * 6;
   arduboy.setCursor(x-totalX/2, y);
   arduboy.print(text);
+}
+
+void buttonsPressed(){
+  if (menu && arduboy.pressed(A_BUTTON)){
+    menu = false;
+  }else if (!menu && arduboy.pressed(B_BUTTON)){
+    menu = true;
+  }
 }
